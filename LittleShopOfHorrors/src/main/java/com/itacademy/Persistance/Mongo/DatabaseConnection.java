9f7579bebc0bd94a3aa0da0cd2ae16e1182fc2ee
TinuxTope <c.tomaspe@gmail.com>
2024-06-16 20:36:34 +0200

    Mongo

--- LittleShopOfHorrors/src/main/java/com/itacademy/Persistance/Mongo/DatabaseConnection.java
+++ LittleShopOfHorrors/src/main/java/com/itacademy/Persistance/Mongo/DatabaseConnection.java
@@ -1,4 +1,34 @@
 package com.itacademy.Persistance.Mongo;
 
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoDatabase;
 public class DatabaseConnection {
+    private static MongoClient mongoClient = null;
+    private static MongoDatabase database = null;
+
+    public static void connect() {
+        if (mongoClient == null) {
+            mongoClient = MongoClients.create("mongodb://localhost:27017");
+            database = mongoClient.getDatabase("thelittleshopofhorrorsdb");
+            System.out.println("Conexión a MongoDB establecida.");
+        }
+    }
+
+    public static void disconnect() {
+        if (mongoClient != null) {
+            mongoClient.close();
+            mongoClient = null;
+            database = null;
+            System.out.println("Conexión a MongoDB cerrada.");
+        }
+    }
+
+    public static MongoDatabase getDatabase() {
+        if (database == null) {
+            connect();
+        }
+        return database;
+    }
+
 }
